82ac6b8c2eadbc55057bc5c6b2a96340b90959db
Frank Bille <github@frankbille.dk>
2012-01-16 23:53:53 +0100

    Added entity information to Player domain object

--- src/main/java/dk/frankbille/scoreboard/domain/Player.java
+++ src/main/java/dk/frankbille/scoreboard/domain/Player.java
@@ -2,6 +2,14 @@ package dk.frankbille.scoreboard.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="player")
 public class Player implements Serializable {
 	private static final long serialVersionUID = 1L;
 
@@ -9,6 +17,8 @@ public class Player implements Serializable {
 
 	private String name;
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	public Long getId() {
 		return id;
 	}
