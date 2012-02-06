a5dfbec4a9c7acf9fd6aa4d8f0dcc98fe73c5991
Frank Bille <github@frankbille.dk>
2012-02-06 15:04:54 +0100

    Use XML instead of annotations for Mappers.
    Currently, you can't do the same in annotations as you can in XML. We
    need to handle both MySQL and HSQLDB (for testing) so we need the new
    databaseId, which is only supported in XML at this point in time.

--- src/main/java/dk/frankbille/scoreboard/dao/mybatis/GameTeamMapper.java
+++ src/main/java/dk/frankbille/scoreboard/dao/mybatis/GameTeamMapper.java
@@ -1,16 +1,11 @@
 package dk.frankbille.scoreboard.dao.mybatis;
 
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Update;
-
 import dk.frankbille.scoreboard.domain.GameTeam;
 
 public interface GameTeamMapper {
 
-	@Insert("INSERT INTO game_team (score, game_id, team_id) VALUES(#{score}, #{game.id}, #{team.id})")
 	int insertGameTeam(GameTeam gameTeam);
 
-	@Update("UPDATE game_team SET score=#{score} WHERE id=#{id}")
 	void updateGameTeam(GameTeam gameTeam);
 
 }
--- src/main/java/dk/frankbille/scoreboard/dao/mybatis/PlayerMapper.java
+++ src/main/java/dk/frankbille/scoreboard/dao/mybatis/PlayerMapper.java
@@ -2,21 +2,14 @@ package dk.frankbille.scoreboard.dao.mybatis;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
-
 import dk.frankbille.scoreboard.domain.Player;
 
 public interface PlayerMapper {
 
-	@Insert("INSERT INTO player (name) VALUES (#{name})")
 	int insertPlayer(Player player);
 
-	@Update("UPDATE player SET name=#{name} WHERE id=#{id}")
 	void updatePlayer(Player player);
 
-	@Select("SELECT	* FROM player ORDER BY name")
 	List<Player> getPlayers();
 
 }
