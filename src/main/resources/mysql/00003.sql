8d6e9c0113caa5e13a3c4bd53e5bd43cef22f91f
Frank Bille <github@frankbille.dk>
2012-02-07 00:24:11 +0100

    Merge Mysql sql files into one
    Changed the jdbc url so we can execute multiple queries in on go. This
    allows us to have a base schema for how it looks currently, but also
    going forward, to keep related schema changes together. This is useful
    if a change causes some data changes.

--- src/main/resources/mysql/00003.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS `team` (
-  `id` bigint(20) NOT NULL auto_increment,
-  `name` varchar(255) default NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB;
--- src/main/resources/mysql/00005.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS `team_players` (
-  `team_id` bigint(20) NOT NULL,
-  `player_id` bigint(20) NOT NULL,
-  PRIMARY KEY  (`team_id`,`player_id`),
-  KEY `IDX_TEAM_ID` (`team_id`),
-  KEY `IDX_PLAYER_ID` (`player_id`),
-  CONSTRAINT `FK_TEAM_PLAYERS_PLAYER_ID` FOREIGN KEY (`player_id`) REFERENCES `player` (`id`),
-  CONSTRAINT `FK_TEAM_PLAYERS_TEAM_ID` FOREIGN KEY (`team_id`) REFERENCES `team` (`id`)
-) ENGINE=InnoDB;
