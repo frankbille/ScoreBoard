f4605fce750afcef6d6e3640ea4addb9703a9a97
Frank Bille <github@frankbille.dk>
2012-02-08 00:13:03 +0100

    Improved the MyBatis mappers so they do what they are supposed to do.

---
+++ src/test/java/dk/frankbille/scoreboard/dao/mybatis/TestMapper.java
@@ -0,0 +1,15 @@
+package dk.frankbille.scoreboard.dao.mybatis;
+
+public interface TestMapper {
+
+	void clearGames();
+
+	void clearGameTeams();
+
+	void clearTeamPlayers();
+
+	void clearTeams();
+
+	void clearPlayers();
+
+}
