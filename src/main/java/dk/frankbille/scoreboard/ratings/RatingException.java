e88fde2c637f7561881658da21f099a9bf458980
Michael Kringelhede <mlp@kringelhede.dk>
2012-02-17 20:09:01 +0100

    Added serialVersionUID

--- src/main/java/dk/frankbille/scoreboard/ratings/RatingException.java
+++ src/main/java/dk/frankbille/scoreboard/ratings/RatingException.java
@@ -2,6 +2,8 @@ package dk.frankbille.scoreboard.ratings;
 
 public class RatingException extends RuntimeException {
 
+	private static final long serialVersionUID = 1L;
+
 	public RatingException(String string) {
 		
 	}
