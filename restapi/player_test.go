8388ab5202e35f26d48d6bf4ab5df52764427b2e
Frank Bille <github@frankbille.dk>
2013-07-02 22:18:59 +0200

    Disable Travis CI, because it can't build Google App Engine applications

--- restapi/player_test.go
+++ restapi/player_test.go
@@ -3,7 +3,7 @@ package scoreboard
 import "testing"
 
 func TestGetPlayerId(t *testing.T) {
-    p := NewPlayer("Some Name", nil, nil)
+    p := NewPlayer("Some Name", "", "")
 
     if p.GetId() != "some-name" {
         t.Errorf("Player.GetId() didn't return the correct slug like ID, based on the name. Expected %v, bus was %v", "some-name", p.GetId())
